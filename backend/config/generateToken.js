5116727c007659e45f66004db56dd8ddf1bf9a07
Amisha Bharti <amishab.ug20.ce@nitp.ac.in>
2024-09-17 01:00:18 +0530

    initial

---
+++ backend/config/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken"); //handle authentication and authorization
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = generateToken;
